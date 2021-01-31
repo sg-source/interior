787761db8a7340d85ba47ea48d890d0173f0376b
Sergey <s_gorokhoff@bk.ru>
2021-01-31 16:50:23 +0300

    test

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -17,20 +17,20 @@ def load_json(file_name):
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        categories = load_json('categories')
-        ProductCategory.objects.all().delete()
-        for cat in categories:
-            new_cat = ProductCategory(**cat)
-            new_cat.save()
-
-        products = load_json('products')
-        Product.objects.all().delete()
-
-        for product in products:
-            category_name = product["category"]
-            _category = ProductCategory.objects.get(name=category_name)
-            product['category'] = _category
-            new_product = Product(**product)
-            new_product.save()
+        # categories = load_json('categories')
+        # ProductCategory.objects.all().delete()
+        # for cat in categories:
+        #     new_cat = ProductCategory(**cat)
+        #     new_cat.save()
+        #
+        # products = load_json('products')
+        # Product.objects.all().delete()
+        #
+        # for product in products:
+        #     category_name = product["category"]
+        #     _category = ProductCategory.objects.get(name=category_name)
+        #     product['category'] = _category
+        #     new_product = Product(**product)
+        #     new_product.save()
 
         super_user = ShopUser.objects.create_superuser('django', 'django@geekshop.local', 'geekbrains', age=33)
